f0f4179eacfd2f97d222f8100206c2ab4c47e82d
Prohor722 <prohor.b722@gmail.com>
2022-02-28 23:51:19 +0600

    phone data passed

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 const searchPhone = () =>{
-    const searchText = document.getElementById('searchText').ariaValueMax;
+    const searchText = document.getElementById('searchText').value;
     fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     .then(res=> res.json())
-    .then(data=>console.log(data))
+    .then(phones=>displayPhones(phones.data));
 }
 
-
+const displayPhones = phones => {
+    console.log(phones);
+}
 {/* <div class="card ms-5" style="width: 18rem;">
     <img src="..." class="card-img-top" alt="...">
     <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     </div>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <a href="#" class="btn btn-brnadColor">Go somewhere</a>
 </div> */}
